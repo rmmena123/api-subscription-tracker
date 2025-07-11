8ee10c250f98d5554fa799667e88b5b9dbbaf4c1
Rodrigo Mena <rodrigo.mena8632@gmail.com>
2025-07-11 16:41:57 -0300

    Feat: error handling middleware

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
