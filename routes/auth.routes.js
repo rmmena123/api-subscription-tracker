2328da647995a541fac1cace306d3a67ad0b53df
Rodrigo Mena <rodrigo.mena8632@gmail.com>
2025-07-14 09:35:38 -0300

    Feat: configuring jwt, controllers and creating signUp controller

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,12 @@
 import { Router } from "express";
+import { signUp, signIn, signOut } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.get("/sign-up", (req, res) => {
-  res.send({ title: "Sign up" });
-});
+authRouter.post("/sign-up", signUp);
 
-authRouter.get("/sign-in", (req, res) => {
-  res.send({ title: "Sign in" });
-});
+authRouter.post("/sign-in", signIn);
 
-authRouter.get("/sign-out", (req, res) => {
-  res.send({ title: "Sign out" });
-});
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
