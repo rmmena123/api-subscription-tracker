b548e982734cc9a35eae4a036aacb05eddc20fe2
Rodrigo Mena <rodrigo.mena8632@gmail.com>
2025-07-15 09:51:11 -0300

    Feat: creating authorize middleware to getUser controller

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # api-subscription-system
 
 Criar novo endereço de IP em "Network Address" se não estiver conectando ao banco de dados Mongooose
+
+Criar documentação técnica ao fim do projeto
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
 import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
 
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => {
   res.send({ title: "CREATE new user" });
